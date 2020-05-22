b1669a3e74c7ef1076a6c83f06851fc82fabbeb9
Chirag Vora <chiragvr669@gmail.com>
2020-05-21 21:29:56 -0400

    initial commit

---
+++ src/main/java/ca/concordia/soen6611/client/Client.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package ca.concordia.soen6611.client;
+
+/**
+ * @author chirag
+ *
+ */
+public class Client {
+	public static void main(final String[] args) {
+	
+	}
+}
---
+++ src/test/java/ca/concordia/soen6611/test/TestsSuite.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package ca.concordia.soen6611.test;
+
+/**
+ * @author chirag
+ *
+ */
+public class TestsSuite {
+
+}
