53c7195d97367c93f0c1cd02de08fd9a65bea4fb
piyushthummar <piyushthummar305@gmail.com>
2020-05-28 14:31:00 -0400

    MAD calculation method added

--- src/main/java/ca/concordia/soen6611/client/CalculateMAD.java
+++ src/main/java/ca/concordia/soen6611/client/CalculateMAD.java
@@ -13,8 +13,19 @@ public class CalculateMAD {
 	/**
 	 * @return the MAD value for given dataset
 	 */
-	public float calculateMeanAbsoluteDivision(ArrayList<Integer> data, float arithmeticMean){
+	public float calculateMeanAbsoluteDivision(ArrayList<Integer> dataset, float arithmeticMean){
 		float meanAbsolteDivision = 0;
+		float sumOfAllData = 0;
+		for(int element : dataset){
+			if(element - arithmeticMean < 0){
+				sumOfAllData = sumOfAllData - (element - arithmeticMean);
+			} else {
+				sumOfAllData = sumOfAllData + (element - arithmeticMean);
+			}
+			sumOfAllData = sumOfAllData +(element - arithmeticMean);
+		}
+		meanAbsolteDivision = sumOfAllData / dataset.size();
+//		System.out.println("MAD is " + meanAbsolteDivision);
 		return meanAbsolteDivision;
 	}
 }
