1fd07b50e2734f9bda132368ab6faaf5bd793f56
piyushthummar <piyushthummar305@gmail.com>
2020-05-28 14:28:12 -0400

    CalculateMAD class created

---
+++ src/main/java/ca/concordia/soen6611/client/CalculateMAD.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ca.concordia.soen6611.client;
+
+import java.util.ArrayList;
+
+/**
+ * @author PIYUSH
+ *
+ */
+public class CalculateMAD {
+	/**
+	 * @return the MAD value for given dataset
+	 */
+	public float calculateMeanAbsoluteDivision(ArrayList<Integer> data, float arithmeticMean){
+		float meanAbsolteDivision = 0;
+		return meanAbsolteDivision;
+	}
+}
