9a81ef467729cfb6ecee91c90bcf9b141fdadb0b
piyushthummar <piyushthummar305@gmail.com>
2020-06-11 15:44:15 -0400

    Some changes of client is completed

--- src/main/java/ca/concordia/soen6611/client/Client.java
+++ src/main/java/ca/concordia/soen6611/client/Client.java
@@ -5,6 +5,7 @@ package ca.concordia.soen6611.client;
 
 import ca.concordia.soen6611.model.StandardDeviation;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import ca.concordia.soen6611.controller.StatisticalController;
 import ca.concordia.soen6611.model.ArithmeticMean;
@@ -35,8 +36,15 @@ public class Client {
 		StandardDeviation standardD = new StandardDeviation();
 		StatisticalController controller=new StatisticalController(calculate, dataSet, aMean, mAD, standardD);
 		
-		ArrayList<Integer> al= dataSet.generateDataset(1000);
-		System.out.println(al.size());
+		System.out.println("Enter the size of dataset(size >= 1000): ");
+		Scanner scan = new Scanner(System.in);
+		int dataSize = scan.nextInt();
+		while(dataSize < 1000){
+			System.out.println("Size is less than 1000. Please enter the size of data-set(>=1000): ");
+			dataSize = scan.nextInt();
+		}
+		ArrayList<Integer> al= dataSet.generateDataset(dataSize);
+		System.out.println("Generated data size is " + al.size());
 		
 		int min = controller.findMinimum(al);
 		int max = controller.findMaximum(al);
@@ -46,14 +54,15 @@ public class Client {
 		float meanAbsoluteDivision = controller.findMeanAbsoluteDivision(al, arithmeticMean);
 		double standardDeviation = controller.findStandardDeviation(al, arithmeticMean);
 	
+		System.out.println("*********************************************");
 		System.out.println("Minimum is " + min);
 		System.out.println("Maximum is " + max);
 		System.out.println("Median is " + median);
 		System.out.println("Mode is " + mode);
-		System.out.println("Arithmetic Mean calculated is " + arithmeticMean);
+		System.out.println("Arithmetic Mean(AM) calculated is " + arithmeticMean);
 		System.out.println("Mean Absolute Division(MAD) is " + meanAbsoluteDivision);
-		System.out.println("StandardDeviation  is " + standardDeviation);
-
+		System.out.println("Standard Deviation is " + standardDeviation);
+		System.out.println("*********************************************");
 
 
 	}
