5f1e7a1321cd0a0769dc18642c7abb52fbb5d601
piyushthummar <piyushthummar305@gmail.com>
2020-06-12 19:15:32 -0400

    TestSuite class updated

--- src/test/java/ca/concordia/soen6611/test/TestsSuite.java
+++ src/test/java/ca/concordia/soen6611/test/TestsSuite.java
@@ -3,10 +3,16 @@
  */
 package ca.concordia.soen6611.test;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
 /**
  * @author chirag
  *
  */
+@RunWith(Suite.class)
+@SuiteClasses({TestArithmeticMean.class, TestMeanAbsoluteDivison.class})
 public class TestsSuite {
 
 }
